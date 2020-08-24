45436504348461f01c1ef5832d472b6b5660c2dc
atreyaray@gmail.com <atreyaray@gmail.com>
2020-08-25 02:23:12 +0530

    bloglist refactored (5.2) bloglist refactored (4.2)

--- part4/index.js
+++ part4/index.js
@@ -1,58 +1,8 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { request } = require('express')
+const config = require('./utils/config')
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// const password = process.env.MONGODB_URI_PASSWORD
-const mongoUrl = process.env.MONGODB_URI
-console.log('url is', mongoUrl,)
-mongoose.connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology:true})
-    .then(result => console.log('connected to MONGODB'))
-    .catch(error => console.log('error',error))
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.get('/api/blogs/:id', (request,response) => {
-    const id = request.params.id
-    Blog
-        .findById(id)
-        .then(blogs => response.json(blogs))
-        .catch(error => console.log(error,))
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    console.log('blog',blog)
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
