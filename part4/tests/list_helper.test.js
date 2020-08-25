954e1dee64cde74ba82df2c759e6597acec29010
atreyaray@gmail.com <atreyaray@gmail.com>
2020-08-25 16:13:40 +0530

    4.5*: helper functions and unit tests, step3

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,5 +1,67 @@
 const listHelper = require('../utils/list_helper')
 
+const blogs = []
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
+const blogsList = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]
+
 test('dummy returns one', () => {
     const blogs = []
 
@@ -8,67 +70,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-    const blogs = []
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ]
-    const blogsList = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-        },
-        {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
-            __v: 0
-        },
-        {
-            _id: "5a422b891b54a676234d17fa",
-            title: "First class tests",
-            author: "Robert C. Martin",
-            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: "5a422ba71b54a676234d17fb",
-            title: "TDD harms architecture",
-            author: "Robert C. Martin",
-            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-            likes: 0,
-            __v: 0
-        },
-        {
-            _id: "5a422bc61b54a676234d17fc",
-            title: "Type wars",
-            author: "Robert C. Martin",
-            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-            likes: 2,
-            __v: 0
-        }
-    ]
 
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes(blogs)
@@ -85,4 +86,30 @@ describe('total likes', () => {
         expect(result).toBe(36)
     })
 
+})
+
+describe('favourite blogs', () => {
+   
+    test('of empty list is null', () => {
+        const result = listHelper.favouriteBlog(blogs)
+        expect(result).toEqual(null)
+    })
+    test('when list only has one blog equals to the blog itself', () => {
+        const correctResponse = {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        }
+        const result = listHelper.favouriteBlog(listWithOneBlog)
+        expect(result).toEqual(correctResponse)
+    })
+    test('of a bigger list is given correctly', () => {
+        const result = listHelper.favouriteBlog(blogsList)
+        const correctResponse = {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        }
+        expect(result).toEqual(correctResponse)
+    })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -12,8 +12,23 @@ const totalLikes = (blogs) => {
         return blogs.map(b => b.likes).reduce(reducer,0)
     }
 }
+const favouriteBlog = (blogs) => {
+    let max = -1
+    let fav = blogs[0]
+    for (var i = 0 ; i < blogs.length ; i++ ){
+        if(blogs[i].likes > max){
+            max = blogs[i].likes
+            fav = blogs[i]
+        }
+    }
+    if (blogs.length === 0)
+        return null
+    else
+        return {title:fav.title, author:fav.author, likes:fav.likes}
+}
 
 module.exports = {
     dummy, 
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
