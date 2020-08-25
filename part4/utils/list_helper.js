e441902c5ab49f0420fb91f21e84cad01ef723ad
atreyaray@gmail.com <atreyaray@gmail.com>
2020-08-25 15:47:49 +0530

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length <= 0){
+        return 0
+    }else{
+        const reducer = (sum,item) => {
+            return sum+item
+        }
+        return blogs.map(b => b.likes).reduce(reducer,0)
+    }
+}
+
 module.exports = {
-    dummy
+    dummy, 
+    totalLikes
 }
